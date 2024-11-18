656410698e4c777dcc9610d2c46f8b21125e111c
Antonio Torquatti <torquattiantonio@gmail.com>
2024-11-18 15:48:50 -0300

    tarea 1

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App =() => {
+  
   return (
     <View style={styles.container}>
-      <Text>Hola Coder!</Text>
-      <StatusBar style="auto" />
-      <Text>
-        Este es un ejemplo de una aplicación de React Native
-      </Text>
+      <View style={styles.contenedorPartes}>
+        <View style={styles.partOne}>
+          <Text style={styles.text}>Hola Coder!</Text>
+        </View>
+        <View style={styles.partTwo}>
+          <Text style={styles.text}>Como estan?</Text>
+        </View>
+      </View>
     </View>
   );
 }
+export default App 
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'red',
+    width: '100%',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  contenedorPartes: {
+    backgroundColor: 'yellow',
+    width: '80%',
+    marginHorizontal: "10%",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  partOne: {
+    backgroundColor: 'blue',
+    //width: '40%',
+    padding: 20,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  partTwo: {
+    backgroundColor: 'green',
+    //width: '40%',
+    padding: 20,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
 });
