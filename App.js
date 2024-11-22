0a3c6114aeafab553c364ccf803648e685515e70
Antonio Torquatti <torquattiantonio@gmail.com>
2024-11-22 10:07:32 -0300

    clase 4

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
 import { useState } from 'react'
-import {Pressable, TextInput, View,Text,StyleSheet} from 'react-native'
+import {Pressable, TextInput, View, Text, StyleSheet, FlatList, Modal} from 'react-native'
 import uuid from 'react-native-uuid'
 
 const App = () => {
@@ -8,6 +8,8 @@ const App = () => {
   const [newValueItem,setNewValueItem] = useState("")
   const [errorInput,setErrorInput] = useState("")
   const [items,setItems] = useState([])
+  const [itemSelected,setItemselected] = useState({})
+  const [modalVisible,setModalVisible]= useState(false)
 
   const addItem = () => {
     if(newValueItem == ""){
@@ -25,6 +27,17 @@ const App = () => {
   const handlerOnChangeItem = (text) => {
     setErrorInput("")
     setNewValueItem(text)
+  }
+
+  const onHandlerModal = (item) => {
+    setItemselected(item)
+    setModalVisible(!modalVisible)
+  }
+
+  const onHandlerDelete = () => {
+    setItems(currentItem => currentItem.filter(item => item.id != itemSelected.id) )
+    setItemselected({})
+    setModalVisible(false)
   }
 
 
@@ -45,13 +58,39 @@ const App = () => {
           <Text style={styles.textButton}>+</Text>
         </Pressable>
       </View>
-      <View style={styles.containerCards}>
-        {items.map((item) => 
-        <View style={styles.card} key={item.id}>
-          <Text style={styles.textCard}>{item.value}</Text>
+      <FlatList
+        data={items}
+        keyExtractor={item => item.id}
+        renderItem={({item})=>{
+          return (
+            <View style={styles.card} key={item.id}>
+              <Text style={styles.textCard}>{item.value}</Text>
+              <Pressable onPress={()=> onHandlerModal(item)} style={styles.button}>
+                <Text style={styles.textButton}>-</Text>
+              </Pressable>
+            </View>
+          )
+        }}
+      />
+    <Modal 
+      animationType='fade' 
+      visible={modalVisible}
+      transparent = {true}
+      >
+        <View style={styles.containerModal}>
+          <View style={styles.contanierSign}>
+            <Text style={styles.modalText}>Esta seguro que quiere eliminar el item: {itemSelected.value }?</Text>
+            <View style={styles.containerButtons}>
+              <Pressable style={styles.modalButton} onPress={onHandlerDelete}>
+                <Text style={styles.modalButtonText}>Si</Text>
+              </Pressable>
+              <Pressable style={styles.modalButton} onPress={() => onHandlerModal({})}>
+                <Text style={styles.modalButtonText}>NO</Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
-      )}
-      </View>
+    </Modal>
     </View>
   )
 }
@@ -60,7 +99,7 @@ export default App
 
 const styles = StyleSheet.create({
   container:{
-    marginTop:30,
+    marginTop:24,
     flex:1
   },
   containerAdd:{
@@ -100,13 +139,48 @@ const styles = StyleSheet.create({
     marginVertical:20,
     padding:15,
     borderRadius:6,
-    alignItems:"center"
+    alignItems:"center",
+    flexDirection:"row"
   },
   textCard:{
-    color:"white"
+    color:"white",
+    flex:1,
+    textAlign:"center"
   },
   error:{
     color:"white"
+  },
+  containerModal:{
+    backgroundColor:"rgba(0,0,0,0.8)",
+    flex:1,
+    justifyContent:"center"
+  },
+  contanierSign:{
+    width:"80%",
+    marginHorizontal:"10%",
+    backgroundColor:"white",
+    padding:30,
+    borderRadius:8,
+    gap:10
+  },
+  modalText:{
+    fontSize:18,
+    textAlign:"center"
+  },
+  containerButtons:{
+    flexDirection:"row"
+  },
+  modalButton:{
+    backgroundColor:"red",
+    padding:20,
+    borderRadius:3,
+    margin:10,
+    flex:1,
+    alignItems:"center"
+  },
+  modalButtonText:{
+    color:"white",
+    fontWeight:"bold"
   }
   
 })
